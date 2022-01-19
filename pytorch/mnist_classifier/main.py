c079ed3e3e031c98fd43783cb9453db587b27dc2
Saravana Rathinam <sr@elbo.ai>
2022-01-19 11:29:28 -0800

    Add accuracy metrics

--- pytorch/mnist_classifier/main.py
+++ pytorch/mnist_classifier/main.py
@@ -11,6 +11,7 @@ from elbo.elbo import ElboModel
 import wandb
 
 wandb.init(project="elbo-mnist-classifier", entity="elbo")
+wandb.login(key=['554f1579018d2fa355625a4c811986e7bc959059'])
 
 
 def get_device():
@@ -50,7 +51,6 @@ class MNISTClassifier(ElboModel, nn.Module):
 
 def train(model, train_dataset, batch_size=200, lr=0.001):
     device = get_device()
-    print(f"Training running on device - {device}")
     model.to(device)
 
     model.train()
@@ -58,7 +58,8 @@ def train(model, train_dataset, batch_size=200, lr=0.001):
     optimizer = torch.optim.Adam(params=model.parameters(), lr=lr)
 
     loss = None
-    for index, (x, y) in tqdm.tqdm(enumerate(train_loader)):
+    y_output = []
+    for index, (x, y) in enumerate(train_loader):
         x = x.to(device)
         y = y.to(device)
 
@@ -69,12 +70,15 @@ def train(model, train_dataset, batch_size=200, lr=0.001):
         loss.backward()
         optimizer.step()
 
-    return loss.detach().cpu().numpy()
+        y_output.append(y_hat)
+
+    return loss.detach().cpu().numpy(), y_output
 
 
 def test(model, test_dataset):
     device = get_device()
     model.to(device)
+    y_output = []
     with torch.no_grad():
         test_loader = torch.utils.data.DataLoader(test_dataset)
         loss = 0.0
@@ -82,15 +86,17 @@ def test(model, test_dataset):
             x = x.to(device)
             y = y.to(device)
             y_hat = model(x)
+            y_output.append(y_hat)
             loss += F.nll_loss(y_hat, y, reduction='sum').detach().cpu().numpy()
 
         loss = loss / len(test_dataset)
         wandb.log({"test_loss": loss})
         print(f"Average Test Loss = {loss}")
+        return y_output
 
 
 if __name__ == '__main__':
-    print(f"Training MNIST classifier")
+    print(f"Training MNIST classifier on {get_device()}")
     _train_data = datasets.MNIST("data", train=True, transform=transforms.ToTensor(), download=True)
     _test_data = datasets.MNIST("data", train=False, transform=transforms.ToTensor(), download=True)
 
@@ -108,8 +114,17 @@ if __name__ == '__main__':
     wandb.watch(_model)
 
     for _epoch in elbo.elbo.ElboEpochIterator(range(0, _num_epochs), _model, save_state_interval=10):
-        _loss = train(_model, _train_data, _batch_size, _lr)
-        wandb.log({"loss": _loss})
-        print(f"Epoch = {_epoch} Loss = {_loss}")
+        _loss, y_train_pred = train(_model, _train_data, _batch_size, _lr)
 
-    test(_model, _test_data)
+        model_output = test(_model, _test_data)
+
+        y_test_pred = torch.argmax(torch.cat(model_output), dim=1)
+        y_test_true = _test_data.test_labels
+
+        y_train_pred = torch.argmax(torch.cat(y_train_pred), dim=1)
+        y_train_true = _train_data.train_labels
+
+        test_accuracy = 100. * (torch.sum(y_test_pred == y_test_true) / (_test_data.data.size(0)))
+        train_accuracy = 100. * (torch.sum(y_train_pred == y_train_true) / (_train_data.data.size(0)))
+        wandb.log({"loss": _loss, "train_accuracy": train_accuracy, "test_accuracy": test_accuracy})
+        print(f"Train accuracy - {train_accuracy} % Test accuracy - {test_accuracy} % Loss = {_loss}")
