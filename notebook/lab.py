a7e48f16fbf9610270ee9d0ca1af9d3179904455
Saravana Rathinam <sr@elbo.ai>
2022-01-19 13:48:21 -0800

    Wait for process

--- notebook/lab.py
+++ notebook/lab.py
@@ -4,12 +4,12 @@ Sample script to start a jupyter lab
 import subprocess
 import sys
 
-TASK_COMMAND = "jupyter lab --no-browser --port=8080 --ip=0.0.0.0"
+TASK_COMMAND = "jupyter lab --allow-root --no-browser --port=8080 --ip=0.0.0.0"
 
 if __name__ == "__main__":
     print(f"jupyter lab is starting ...")
     args_list = list(TASK_COMMAND.split(' '))
-
+    process = None
     try:
         process = subprocess.Popen(args_list,
                                    stdout=subprocess.PIPE,
@@ -21,3 +21,6 @@ if __name__ == "__main__":
 
     for c in iter(lambda: process.stdout.read(1), b''):
         sys.stdout.buffer.write(c)
+
+    if process:
+        process.wait()
